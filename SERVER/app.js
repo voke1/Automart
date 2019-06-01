7fa35fc29c4b73e105ccac8f42acb8d3e1fe4cdf
Voke Olomu <vokeolomu@yahoo.com>
2019-06-01 18:00:40 +0100

    update feature with controller and model file

--- SERVER/app.js
+++ SERVER/app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 
 const app = express();
-const orderRoutes =  require("./routes/cars");
+const carRoutes =  require("./routes/car_route");
 
 
 app.use(bodyParser.json());
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(logger("dev"));
 
 
-app.use(("/car", orderRoutes));
+app.use(("/car", carRoutes));
 
 module.exports = app;
 
---
+++ SERVER/routes/car_route.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+import Car from '../controller/car_controller';
+
+router.get('/cars', Car.getAll);
+
+
+
+module.exports = router;
+
