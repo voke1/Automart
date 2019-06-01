e34452af8f54fec64385486be4eb7747f25cc02e
Voke Olomu <vokeolomu@yahoo.com>
2019-06-01 09:07:33 +0100

    create api to check cars within certain price range

---
+++ SERVER/routes/cars.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/car", (req, res, next) => {
+    res.status(200).json({
+        message: "Handling GET request to /car"
+    })
+});
+
+router.get("/car?status=available&min_price= XXXValue &max_price= XXXValue", (req, res, next) => {
+    const id = req.params.carId;
+
+    res.status(200).json({
+
+        status: 200,
+        data: [
+            {
+            id: id,
+            created_on: 'DateTime',
+            manufacturer: 'String',
+            model: 'String',
+            price: 'Float',
+            state: 'String',
+            status: "String",
+            body_type: "string"
+        },
+        {
+            id: id,
+            created_on: 'DateTime',
+            manufacturer: 'String',
+            model: 'String',
+            price: 'Float',
+            state: 'String',
+            status: "String",
+            body_type: "string"
+        }
+    ]
+
+    })
+});
+
+module.exports = router;
+
