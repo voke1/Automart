9536a6cbda4d35fbc7dee55d09caab389431f647
Voke Olomu <vokeolomu@yahoo.com>
2019-06-05 06:55:52 +0100

    add feature to view all available ads by price filter

---
+++ SERVER/controller/car_controller.js
@@ -0,0 +1,34 @@
+import CarModel from '../model/car_model';
+
+const Car = {
+  /**
+   * 
+   * @param {object} req 
+   * @param {object} res
+   * @returns {object} car object 
+   */
+  create(req, res) {
+    if (!req.body.success && !req.body.lowPoint && !req.body.takeAway) {
+      return res.status(400).send({'message': 'All fields are required'})
+    }
+
+    
+    const car = CarModel.create(req.body);
+    return res.status(201).send(car);
+  },
+  /**
+   * 
+   * @param {object} req 
+   * @param {object} res 
+   * @returns {object} car array
+   */
+  getAll(req, res) {
+    const cars = CarModel.findAll();
+    return res.status(200).send(cars);
+  }
+}
+
+export default Car;
+
+
+    
