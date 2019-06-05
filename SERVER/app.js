041c2253d6789c89c397500784174c11b4a157fa
Voke Olomu <vokeolomu@yahoo.com>
2019-06-05 06:24:02 +0100

    refactor app.js

--- SERVER/app.js
+++ SERVER/app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 
 const app = express();
-const carRoutes =  require("./routes/car_route");
+const carRoutes =  require ("./routes/car_route");
 
 
 app.use(bodyParser.json());
