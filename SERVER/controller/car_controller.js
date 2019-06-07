cc70ee36602277e4f8f29d8d29df1e33bed79713
Voke Olomu <vokeolomu@yahoo.com>
2019-06-07 13:12:20 +0100

    refactor code

--- SERVER/controller/car_controller.js
+++ SERVER/controller/car_controller.js
@@ -1,21 +1,7 @@
 import CarModel from '../model/car_model';
 
 const Car = {
-  /**
-   * 
-   * @param {object} req 
-   * @param {object} res
-   * @returns {object} car object 
-   */
-  create(req, res) {
-    if (!req.body.success && !req.body.lowPoint && !req.body.takeAway) {
-      return res.status(400).send({'message': 'All fields are required'})
-    }
 
-    
-    const car = CarModel.create(req.body);
-    return res.status(201).send(car);
-  },
     /**
    * 
    * @param {object} req 
