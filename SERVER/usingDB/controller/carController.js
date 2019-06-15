2a3d5ab6ef757d4a1040b40a161a14c6c68716e6
Voke Olomu <vokeolomu@yahoo.com>
2019-06-15 21:52:40 +0100

    Add feature to view all cars sold or unsold

--- SERVER/usingDB/controller/carController.js
+++ SERVER/usingDB/controller/carController.js
@@ -1,6 +1,22 @@
 import db from '../db';
 
 const Car = {
+
+    /**
+   * Get All Car
+   * @param {object} req 
+   * @param {object} res 
+   * @returns {object} cars array
+   */
+  async getAll(req, res) {
+    const findAllQuery = 'SELECT * FROM cars';
+    try {
+      const { rows, rowCount } = await db.query(findAllQuery);
+      return res.status(200).send({ rows, rowCount });
+    } catch(error) {
+      return res.status(400).send({status: 400, error});
+    }
+  },
  /*
    * Delete A Car
    * @param {object} req 
