80f7f0df79d829569777e6da2b7df78d04720a44
Voke Olomu <vokeolomu@yahoo.com>
2019-06-15 21:16:24 +0100

    create function to delete specific car

---
+++ SERVER/usingDB/controller/carController.js
@@ -0,0 +1,24 @@
+import db from '../db';
+
+const Car = {
+ /*
+   * Delete A Car
+   * @param {object} req 
+   * @param {object} res 
+   * @returns {void} return statuc code 204 
+   */
+  async delete(req, res) {
+    const deleteQuery = 'DELETE FROM cars WHERE id=$1 returning *';
+    try {
+      const { deletedCar } = await db.query(deleteQuery, [req.params.id]);
+      if(!deletedCar[0]) {
+        return res.status(404).send({status: 404, error: 'no car to delete'});
+      }
+      return res.status(204).send({status: 204, message: "Car Ad successfully deleted" });
+    } catch(error) {
+      return res.status(400).send({status: 400, error});
+    }
+  }
+}
+
+export default Car;
