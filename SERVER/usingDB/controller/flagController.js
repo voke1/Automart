4799bf67c49af1632308440366d565533970f28a
Voke Olomu <vokeolomu@yahoo.com>
2019-06-15 21:45:37 +0100

    Add function to report/flag a posted Car Ad as fraudulent

---
+++ SERVER/usingDB/controller/flagController.js
@@ -0,0 +1,36 @@
+import moment from 'moment';
+import uuidv4 from 'uuid/v4';
+import db from '../db';
+
+const Flag = {
+  /**
+   * Create A Flag
+   * @param {object} req 
+   * @param {object} res
+   * @returns {object} flag object 
+   */
+  async create(req, res) {
+    const text = `INSERT INTO
+      flags(id, car_id, reason, description, created_on, modified_date)
+      VALUES($1, $2, $3, $4, $5, $6)
+      returning *`;
+    const values = [
+      uuidv4(),
+      req.body.car_id,
+      req.body.reason,
+      req.body.description,
+      moment(new Date()),
+      moment(new Date())
+    ];
+
+    try {
+      const { rows } = await db.query(text, values);
+      return res.status(201).send(rows[0]);
+    } catch(error) {
+      return res.status(400).send({status: 400, error});
+    }
+  },
+  
+}
+
+export default Flag;
