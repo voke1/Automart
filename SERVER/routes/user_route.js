2651f197f942bdad0bf3b148eb4db7e0f56eb675
Voke Olomu <vokeolomu@yahoo.com>
2019-07-03 19:38:34 +0100

    feat(sign-in.js): update code for user sign-in

--- SERVER/routes/user_route.js
+++ SERVER/routes/user_route.js
@@ -7,7 +7,7 @@ import Auth from '../usingDB/middleware/checkAuth';
 const router = express.Router();
 
 // sign in user
-router.post('/api/v1/auth/signin', Auth, User.getOne);
+router.post('/api/v1/auth/signin', User.getOne);
 
 // sign up a user
 router.post('/api/v1/auth/signup', User.create);
