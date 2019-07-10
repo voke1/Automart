48d0f84349e31c30487f7faaba8795bae8b03730
Voke Olomu <vokeolomu@yahoo.com>
2019-07-10 18:02:23 +0100

    feat(UI): add features for user to view all ads

---
+++ UI/js/view-ads.js
@@ -0,0 +1,48 @@
+async function viewAds() {
+  // e.preventDefault();
+
+
+  if (!(localStorage.getItem('authorization'))) {
+    // eslint-disable-next-line no-alert
+    alert('Please sign in');
+    window.location.href = 'sign-in.html';
+  }
+
+  const response = await fetch('http://localhost:2000/api/v1/car', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-type': 'application/json',
+      authorization: localStorage.getItem('authorization'),
+    },
+
+
+  });
+  const data = await response.json();
+  console.log(data.rows);
+  if (data.rows.length > 0) {
+    let adTemplate;
+    const adList = document.querySelector('.column');
+
+    for (let i = 0; i < data.rows.length; i++) {
+      adTemplate += ` <li>
+      <div class="img-i">
+      <label class="car-state-label">NEW</label>
+          <a href="car-details.html"><img src="./img/Benz.png" alt="product1"></a>
+      </div>
+    
+      <div class="desc-i">
+          <ul class='car-list'>
+              <li>&nbsp; NAME: G-WAGON</li>
+              <li>&nbsp; PRICE: NGN34,234,230.00</li>
+          </ul>
+          <h3 class="view-btn"><a href="car-details.html">VIEW CAR</a></h3>
+    
+      </div>
+    </li>`;
+    }
+
+    adList.innerHTML = adTemplate;
+  }
+}
+viewAds();
