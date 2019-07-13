90007dc9bcbc8aeb3e916c5422eb329b33389b92
Voke Olomu <vokeolomu@yahoo.com>
2019-07-13 23:15:07 +0100

    chore: refactor code to align with required specifications

--- SERVER/usingDB/middleware/checkAuth.js
+++ SERVER/usingDB/middleware/checkAuth.js
@@ -3,8 +3,8 @@ import jwt from 'jsonwebtoken';
 module.exports = (req, res, next) => {
   try {
     // const token = req.body.authorization;
-    const token = req.headers.authorization;
-    const decode = jwt.verify(token, process.env.TOKEN);
+    const auth = req.headers.token;
+    const decode = jwt.verify(auth, process.env.TOKEN);
     req.userData = decode;
     next();
   } catch (error) {
